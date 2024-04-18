67229ca2a1fd5c2948d8494ec1009f56f2368c5e
Badre Tejado-Imam <btejado@btejado-thinkpadx1nanogen2.bos.csb>
2024-04-18 16:36:14 -0400

    Added multi var regression example and separated in folders

---
+++ multi_var_regression/linear_regression_multiple_variables.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import numpy as np
+from sklearn import linear_model
+import math
+
+df = pd.read_csv("homeprices2.csv")
+
+# To fill the missing value, we calculate the median of the bedrooms column
+median_bedrooms = math.floor(df.bedrooms.median())
+df.bedrooms = df.bedrooms.fillna(median_bedrooms)
+print(df)
+
+# Create and train the model
+reg = linear_model.LinearRegression()
+reg.fit(df[['area', 'bedrooms', 'age']], df.price)
+print("The coefficients are: ", reg.coef_)
+print("The intercept is: ", reg.intercept_)
+
+# Make a prdiction
+pred = reg.predict([[3000, 3, 40]])
+print("The prediction for a 3000sqft, 3bdr, 40yr house is: ", pred)
